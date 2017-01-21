d46a2b21f2dccf93cc66bd7add62d2935a907261
Barney Carroll <barney.carroll@gmail.com>
2017-01-21 17:53:14 +0000

    Tweak return processing logic

--- mimix.js
+++ mimix.js
@@ -11,15 +11,24 @@ function mimix( attrs_list ){
     : typeof values[ 0 ] === 'function'
     ? function(){
         var outputs = []
+        var allFalse = true
+        var promises = false
 
         for( var i = 0; i < values.length; i++ ){
           outputs[ i ] = values[ i ].apply( this, arguments )
 
-          if( outputs[ i ] === false )
-            return false
+          if( outputs[ i ] != false ){
+            allFalse = false
+
+            if( !promises && typeof outputs[ i ] != 'undefined' && typeof outputs[ i ].then )
+              promises = true
+          }
         }
 
-        if( outputs[ i ].then )
+        if( allFalse )
+          return false
+
+        else if( promises )
           return Promise.all( outputs )
 
         else
@@ -38,5 +47,5 @@ function mimix( attrs_list ){
   return output
 }
 
-if( typeof module !== 'undefined' )
-  module[ 'exports' ] = mimix
+if( typeof module != 'undefined' ) module[ 'exports' ] = mimix
+else if( typeof window != 'undefined' ) window.mimix = mimix
